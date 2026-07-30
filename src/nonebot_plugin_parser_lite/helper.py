06e7721f4a501eb9f9dfa271b5a0dac2649141eb
Stlara-F <130404897+Stlara-F@users.noreply.github.com>
2026-07-30 19:43:35 +0800

    fix: subscriptable stubs for helper.py standalone (UniMessage[Any])

--- src/nonebot_plugin_parser_lite/helper.py
+++ src/nonebot_plugin_parser_lite/helper.py
@@ -7,22 +7,27 @@ from anyio import Path
 from .utils._flags import _STANDALONE
 
 if _STANDALONE:
-    # Stubs for standalone mode: 所有 nonebot_plugin_alconna.uniseg 导入类型
-    Event = object          # type: ignore
-    Segment = object        # type: ignore
-    Reference = object      # type: ignore
-    UniMessage = object     # type: ignore
-    UniHelper = object      # type: ignore
-    CustomNode = object     # type: ignore
-    Image = object          # type: ignore
-    Video = object          # type: ignore
-    File = object           # type: ignore
-    Voice = object          # type: ignore
-    Text = object           # type: ignore
-    uniseg = None           # type: ignore
-    SupportAdapter = object # type: ignore
-    current_bot = None      # type: ignore
-    current_event = None    # type: ignore
+    # Standalone stubs: 所有 nonebot_plugin_alconna.uniseg 类型
+    # 使用带 __class_getitem__ 的类以支持 UniMessage[Any] 等下标语法
+    class _Stub:
+        def __class_getitem__(cls, _item): return cls
+        def __init_subclass__(cls, **kwargs): pass
+
+    class Segment(_Stub): pass      # type: ignore
+    class UniMessage(_Stub): pass   # type: ignore
+    class UniHelper: pass           # type: ignore
+    class CustomNode(_Stub): pass   # type: ignore
+    class Image(_Stub): pass        # type: ignore
+    class Video(_Stub): pass        # type: ignore
+    class File(_Stub): pass         # type: ignore
+    class Voice(_Stub): pass        # type: ignore
+    class Text(_Stub): pass         # type: ignore
+    class Reference(_Stub): pass    # type: ignore
+    class Event: pass               # type: ignore
+    SupportAdapter = object         # type: ignore
+    uniseg = None                   # type: ignore
+    current_bot = None              # type: ignore
+    current_event = None            # type: ignore
 else:
     from nonebot.adapters import Event
     from nonebot.matcher import current_bot, current_event
